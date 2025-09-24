294a9e00edf6fd8fd1877d6d6fba4a8b923336c1
jayachandrasomireddygari <jayachandra.somireddygari>
2025-09-24 12:45:49 +0530

    debug changed

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e9k@@)$^*#gjj5*r+(wo1fl@stp6pb!l^-ds5g5)g+d17$06#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
